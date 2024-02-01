ef9e93a205e7332eba0f451ec8c792bd63c45478
Sergey <sergey@example.com>
2024-02-01 08:50:11 +0300

    02 number tasks 3

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -113,11 +113,7 @@ function getLinearEquationRoot(a, b) {
 function getAngleBetweenVectors(x1, y1, x2, y2) {
     const numerator = x1*x2 + y1*y2;
     const denominator = Math.hypot(x1, y1) * Math.hypot(x2, y2);
-    
-    // Вычисляем угол между векторами
     const angle = Math.acos(numerator / denominator);
-    
-    // Возвращаем угол в радианах
     return angle;
 }
 
